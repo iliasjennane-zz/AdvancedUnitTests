eab85a13274f0c7272878890869c1d16b6b649cf
Ilias Jennane <iliasj@microsoft.com>
2017-08-18 16:11:04 +0000

    Merged PR 45: Added initial database + database project
    Added initial database + database project
    
    Related work items: #559

---
+++ SUT.DataAccess/db.cs
@@ -0,0 +1,19 @@
+﻿using SUT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUT.DataAccess
+{
+    public class db:DbContext
+    {
+        public db():base("name=SUT.Database")
+        {
+
+        }
+        public virtual DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ SUT.Database/dbo/Tables/Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]                   INT           IDENTITY (1, 1) NOT NULL,
+    [Name]                 VARCHAR (MAX) NOT NULL,
+    [Salary]               FLOAT (53)    NOT NULL,
+    [HireDate]             DATETIME2 (7) NOT NULL,
+    [PerformanceStarLevel] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
