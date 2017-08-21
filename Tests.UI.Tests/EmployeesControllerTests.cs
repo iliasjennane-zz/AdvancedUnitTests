63d22ce2b0724fa6e89267ee2aacb0437a023a67
Ilias Jennane <iliasj@microsoft.com>
2017-08-21 13:17:52 +0000

    Merged PR 48: Added DB dependency to the SUT, and Added DB Dependency to the UT.
    Added DB dependency to the SUT, and Added DB Dependency to the UT.
    
    Related work items: #554

---
+++ Tests.UI.Tests/EmployeesControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SUT.UI.Controllers;
+using System.Web.Mvc;
+using SUT.Entities;
+using System.Collections.Generic;
+using SUT.DataAccess;
+using System.Linq;
+
+namespace Tests.UI.Tests
+{
+    [TestClass]
+
+    public class EmployeesControllerTests
+    {
+        [TestMethod]
+        [TestCategory("UI.Logic.UnitTests")]
+        public void TestIndex_ShouldReturnTheIndexViewWithAListOfEmployeesAsAModel()
+        {
+            //AAA
+            //Arrange
+            EmployeesController controllerUnderTest = new EmployeesController();
+            //Act
+            var returnedResult = controllerUnderTest.Index().Result;
+            //Assert
+            Assert.AreEqual(returnedResult.GetType(), typeof(ViewResult));
+            var returnedView = returnedResult as ViewResult;
+            Assert.AreEqual(returnedView.Model.GetType(), typeof(List<Employee>));
+        }
+
+        [TestMethod]
+        [TestCategory("UI.Logic.UnitTests")]
+        public void TestCreate_ShouldAddAnEmployeeToTheDatabase()
+        {
+            //AAA
+            //Arrange
+            EmployeesController controllerUnderTest = new EmployeesController();
+            string employeeTestName = "Test Employee " + DateTime.Now.ToLongTimeString();
+            var employeeToBeAdded = new Employee { Name = employeeTestName, Salary = 100000, HireDate = DateTime.Now, PerformanceStarLevel = 1 };
+            //Act
+            var returnedResult = controllerUnderTest.Create(employeeToBeAdded).Result;
+            //Assert
+            Assert.AreEqual(returnedResult.GetType(), typeof(RedirectToRouteResult));
+            var returnedView = returnedResult as RedirectToRouteResult;
+            Assert.AreEqual(returnedView.RouteValues["Action"], "Index");
+
+            //Need to verify that the employee has been Added.
+            using (db dbContext = new db())
+            {
+                var employeeInserted = dbContext.Employees.Where(e => e.Name == employeeTestName).FirstOrDefault();
+                Assert.IsNotNull(employeeInserted);
+            }
+        }
+    }
+}
---
+++ Tests.UI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests.UI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests.UI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13bc7797-aa89-420c-bb2c-61b89c3f487c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
